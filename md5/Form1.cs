86344bd19b49e04cc09af87ed4ae6fed89586174
Alper <jstforvenus@gmail.com>
2017-10-24 05:45:06 +0300

    md şifreleme yapılıyor (kontrol için 1223456 mesajı var)

--- md5/Form1.cs
+++ md5/Form1.cs
@@ -16,5 +16,26 @@ namespace md5
         {
             InitializeComponent();
         }
+        string sifrele;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sifrele=SifreleM.Md5Sifreleme(textBox2.Text);
+            label1.Text = "Oluşturulan Hash..:"+sifrele;
+
+            if (textBox2.Text=="123456")
+            {
+            label2.Text = "Hash 123456: "+"e10adc3949ba59abbe56e057f20f883e";
+            MessageBox.Show("123456 doğru şifrelendi");
+            }
+            
+        
+
+
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.Text="";
+        }
     }
 }
---
+++ md5/SifreleM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace md5
+{
+    class SifreleM
+    {
+        static public string Md5Sifreleme(string metin)
+        {
+
+            MD5CryptoServiceProvider Md5 = new MD5CryptoServiceProvider();
+            byte[] bte = Encoding.UTF8.GetBytes(metin);
+            bte=Md5.ComputeHash(bte);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte ba in bte)
+            {
+                sb.Append(ba.ToString("x2").ToLower());
+            }
+            return sb.ToString();
+
+        }
+
+
+    }
+}
